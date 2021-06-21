d329b412015b440aae33b6a84120974c4fa405da
Independent-Dev <juju08217@daum.net>
2021-06-21 15:04:45 +0900

    backtracking problem solved

---
+++ algorithm/backtracking/15652.py
@@ -0,0 +1,15 @@
+# 210621 N과 M silver3
+# backtracking
+def solve(length):
+    if length == M:
+        print(' '.join(list(map(str, arr[1:]))))
+        return
+
+    for n in range(1, N + 1):
+        if arr[length] <= n:
+            arr[length + 1] = n
+            solve(length + 1)
+
+N, M = list(map(int, input().split()))
+arr = [0] * (M + 1)
+solve(0)
