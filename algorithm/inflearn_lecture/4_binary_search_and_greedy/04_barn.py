2a4ebfb50c9ac802c6a598d03a97a7b545fb3eec
Independent-Dev <juju08217@daum.net>
2021-07-02 00:37:48 +0900

    little optimization for binary search 04

--- algorithm/inflearn_lecture/4_binary_search_and_greedy/04_barn.py
+++ algorithm/inflearn_lecture/4_binary_search_and_greedy/04_barn.py
@@ -10,37 +10,32 @@ def solution(**kwargs):
     N, C = map(int, input().split())
     barns = sorted([int(input()) for _ in range(N)])
     barn_gap = list(barns[i] - barns[i - 1] for i in range(1, N))
-    start, stop = min(barns[i] - barns[i - 1] for i in range(1, N)), barns[-1] - barns[0]
+    start, stop = min(barn_gap), sum(barn_gap)
     result = 0
-    while start <= stop:  # 이 while문 안의 기본 구조를 잘 기억하는 것이 중요하겠다...!!
+    while start <= stop:  # TODO 이 while문 안의 기본 구조를 잘 기억하는 것이 중요하겠다...!!
         mid = (start + stop) // 2
-        if Count(mid, barn_gap) < C:
+        horse_count, temp_result = calculate(mid, barn_gap)
+        if horse_count < C:
             stop = mid - 1
         else:
             start = mid + 1
-            result = max(result, max_gap(mid, barn_gap))
+            result = max(result, temp_result)
     return [str(result)]
 
-def Count(mid, barn_gap):
-    result = 1
-    temp = 0
-    for gap in barn_gap:
-        if temp + gap >= mid:
-            result += 1
-            temp = 0
-        else:
-            temp += gap
-    return result
 
-def max_gap(mid, barn_gap):
-    temp_min = sum(barn_gap)
-    temp = 0
+def calculate(mid, barn_gap):
+    temp_result = sum(barn_gap)
+    horse_count = 1
+    horse_gap = 0
     for gap in barn_gap:
-        temp += gap
-        if temp >= mid:
-            temp_min = min(temp_min, temp)
-            temp = 0
-    return temp_min
+        horse_gap += gap
+        if horse_gap >= mid:
+            temp_result = min(temp_result, horse_gap)
+            horse_count += 1
+            horse_gap = 0
+
+    return horse_count, temp_result
+
 
 if __name__ == "__main__":
     print(solution(examine=True))
