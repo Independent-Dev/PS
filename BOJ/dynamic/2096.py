227d40cdb4f8615988ef9b902efbce87df0f74a5
Independent-Dev <juju08217@daum.net>
2021-09-26 14:41:45 +0900

    2096 더 깔끔하게 다시 풀음

--- BOJ/dynamic/2096.py
+++ BOJ/dynamic/2096.py
@@ -1,20 +1,20 @@
 # 210925 내려가기 gold4
+# 210926 다시 풀음
 import sys, copy
 
 input = sys.stdin.readline
+function_list = [max, min]
 
 N = int(input())
 res_list = [[0, 0, 0], [0, 0, 0]]
 for _ in range(N):
     a, b, c = list(map(int, input().split()))
     temp = copy.deepcopy(res_list)
-    res_list[0][0] = a + min(temp[0][0], temp[0][1])
-    res_list[0][1] = b + min(temp[0][0], temp[0][1], temp[0][2])
-    res_list[0][2] = c + min(temp[0][1], temp[0][2])
-    res_list[1][0] = a + max(temp[1][0], temp[1][1])
-    res_list[1][1] = b + max(temp[1][0], temp[1][1], temp[1][2])
-    res_list[1][2] = c + max(temp[1][1], temp[1][2])
+    for i, function in enumerate(function_list):
+        res_list[i][0] = a + function(temp[i][0], temp[i][1])
+        res_list[i][1] = b + function(temp[i][0], temp[i][1], temp[i][2])
+        res_list[i][2] = c + function(temp[i][1], temp[i][2])
 
-print(max(res_list[1]), min(res_list[0]))
+print(max(res_list[0]), min(res_list[1]))
 
 
