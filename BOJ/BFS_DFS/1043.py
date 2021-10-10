722550706e7eb187cf61768cac43b185591de01c
Independent-Dev <juju08217@daum.net>
2021-10-10 22:45:06 +0900

    BOJ 1043(BFS) solved again

--- BOJ/BFS_DFS/1043.py
+++ BOJ/BFS_DFS/1043.py
@@ -5,7 +5,7 @@ import sys
 input = sys.stdin.readline
 
 N, M = list(map(int, input().split()))
-prev, cur = M, 0
+cur = M
 
 knowing_people =list(map(int, input().split()))
 
@@ -14,15 +14,17 @@ party_people = list()
 for _ in range(M):
     party_people.append(set(map(int, input().split()[1:])))
 
+visited = [0] * len(party_people)
+
 if len(knowing_people) == 1:
     print(M)
 else:
     knowing_people = set(knowing_people[1:])
-    while prev != cur:
-        prev = cur
+    while cur:
         cur = 0
-        for people in party_people:
-            if people & knowing_people:
+        for i, people in enumerate(party_people):
+            if not visited[i] and people & knowing_people:
                 knowing_people |= people
                 cur += 1
-    print(M - cur)
+                visited[i] = 1
+    print(M - sum(visited))
