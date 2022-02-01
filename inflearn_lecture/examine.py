d2247450ba3bb2458902d3549ec404151777e34a
Independent-Dev <juju08217@daum.net>
2022-02-01 19:52:15 +0900

    examine.py file. 잃어버리지 않도록...

---
+++ inflearn_lecture/examine.py
@@ -0,0 +1,27 @@
+import os, functools
+
+
+def examine(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        if kwargs['examine']:
+            count, output = 1, []
+            folder_path = os.getcwd()
+            while os.path.isfile(f"{folder_path}/out{count}.txt"):
+                f = open(f"{folder_path}/out{count}.txt", 'r')
+                for line in f.readlines():
+                    output.append(line.split())
+                count += 1
+
+            for i, o in enumerate(output, start=1):
+                func_output = function(**kwargs, folder_path=os.getcwd(), id=i)
+                if func_output == o:
+                    print(f"#{i} = success!!")
+                else:
+                    print(f"#{i} = not success!!")
+                    print(f"function(): {function(**kwargs, folder_path=os.getcwd(), id=i)}")
+                print(f"output: {o}")
+        else:
+            return function(*args, **kwargs)
+
+    return wrapper
