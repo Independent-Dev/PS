9be00459785181cc58c497f94dd7834a4852cddb
Independent-Dev <juju08217@daum.net>
2022-02-12 16:06:40 +0900

    examine.py file. updated for easy understanding of result

--- inflearn_lecture/examine.py
+++ inflearn_lecture/examine.py
@@ -9,19 +9,24 @@ def examine(function):
             folder_path = os.getcwd()
             while os.path.isfile(f"{folder_path}/out{count}.txt"):
                 f = open(f"{folder_path}/out{count}.txt", 'r')
+                temp = []
                 for line in f.readlines():
                     if line.strip():
-                        output.append(line.split())
+                        temp.append(line.strip())
+                output.append(temp)
                 count += 1
 
+            success_count = 0
             for i, o in enumerate(output, start=1):
                 func_output = function(**kwargs, folder_path=os.getcwd(), id=i)
                 if func_output == o:
+                    success_count += 1
                     print(f"#{i} = success!!")
                 else:
                     print(f"#{i} = not success!!")
                     print(f"function(): {func_output}")
                 print(f"output: {o}")
+            print(f"success: {success_count} / {len(output)}")
         else:
             return function(*args, **kwargs)
 
