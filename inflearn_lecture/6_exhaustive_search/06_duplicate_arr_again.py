c40924f89c277437449c09b8d632dc6e1987f092
Independent-Dev <juju08217@daum.net>
2022-03-24 22:58:53 +0900

    section6 exhaustive search 06 solved again

---
+++ inflearn_lecture/6_exhaustive_search/06_duplicate_arr_again.py
@@ -0,0 +1,25 @@
+# 210727
+from examine.examine import examine
+import sys
+
+@examine
+def solution(**kwargs):
+    if kwargs['examine']:
+        sys.stdin = open(f"{kwargs['folder_path']}/in{kwargs['id']}.txt", 'r')
+
+    def dfs(x=0, arr=[]):
+        if x == M:
+            res.append(' '.join(arr))
+        else:
+            for i in range(1, N + 1):
+                dfs(x + 1, arr + [str(i)])
+
+    N, M = [int(x) for x in input().split()]
+    res = []
+    dfs()
+
+    return res + [str(len(res))]
+
+
+if __name__ == "__main__":
+    print(solution(examine=True))
