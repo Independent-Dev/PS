970203ecc47f5fe848a805421e4b53d313776cdc
Jinoh Kim <juju08217@daum.net>
2022-04-25 16:18:03 +0900

    section6 exhaustive search 11 solved again

---
+++ inflearn_lecture/6_exhaustive_search/11_combination_sum_again.py
@@ -0,0 +1,26 @@
+from examine import examine
+import sys, math
+
+@examine
+def solution(**kwargs):
+    if kwargs['examine']:
+        sys.stdin = open(f"{kwargs['folder_path']}/in{kwargs['id']}.txt", 'r')
+
+    def dfs(idx=0, count=0, sum=0):
+        if count == M:
+            if sum % divide == 0:
+                nonlocal res
+                res += 1
+        elif idx < N:
+            dfs(idx + 1, count + 1, sum + target[idx])
+            dfs(idx + 1, count, sum)
+
+
+    N, M = [int(x) for x in input().split()]
+    target = [int(x) for x in input().split()]
+    divide = int(input())
+    res = 0
+    dfs()
+
+if __name__ == "__main__":
+    print(solution(examine=True))
