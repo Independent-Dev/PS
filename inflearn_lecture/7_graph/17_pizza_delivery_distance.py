85181b9da5d19a28ee97bd1045a3a8c35650b157
Jinoh Kim <juju08217@daum.net>
2022-05-24 13:23:09 +0900

    section7 graph search 17

--- inflearn_lecture/7_graph/17_pizza_delivery_distance.py
+++ inflearn_lecture/7_graph/17_pizza_delivery_distance.py
@@ -1,25 +1,11 @@
 from examine import examine
-import sys, pprint
-
-OFFSET_LIST = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+import sys
+from itertools import combinations
 
 @examine
 def solution(**kwargs):
     if kwargs['examine']:
         sys.stdin = open(f"{kwargs['folder_path']}/in{kwargs['id']}.txt", 'r')
-
-    def dfs(idx=0, city_pizza_distance=0):
-        nonlocal result
-        if not result:
-            if idx == len(house_list):
-                if sum(visited) <= M:
-                    result = city_pizza_distance
-            else:
-                for distance, pizza_store_idx in pizza_distance_list[idx]:
-                    visited[pizza_store_idx] = 1
-                    dfs(idx + 1, city_pizza_distance + distance)
-                    visited[pizza_store_idx] = 0
-
 
     N, M = [int(num) for num in input().split()]
     city_map = []
@@ -35,17 +21,22 @@ def solution(**kwargs):
             elif city_map[-1][x] == 2:
                 pizza_store_list.append([y, x])
 
-    visited = [0] * len(pizza_store_list)
-
     for house_y, house_x in house_list:
-        pizza_distance_list.append([])
+        pizza_distance_list.append({})
         for idx, (pizza_store_y, pizza_store_x) in enumerate(pizza_store_list):
-            pizza_distance_list[-1].append([abs(house_y - pizza_store_y) + abs(house_x - pizza_store_x), idx])
-        pizza_distance_list[-1].sort()
+            pizza_distance_list[-1][idx] = abs(house_y - pizza_store_y) + abs(house_x - pizza_store_x)
 
-    result = None
+    result = 2 * N * len(house_list)
 
-    dfs()
+    for pizza_store_com in list(combinations(range(len(pizza_store_list)), M)):
+        temp = 0
+        for pizza_distance in pizza_distance_list:
+            min_distance = N + N
+            for pizza_store in pizza_store_com:
+                min_distance = min(min_distance, pizza_distance[pizza_store])
+            temp += min_distance
+        result = min(temp, result)
+
     return [str(result)]
 
 if __name__ == "__main__":
