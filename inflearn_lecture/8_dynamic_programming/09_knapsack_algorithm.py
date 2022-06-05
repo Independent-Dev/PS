093415e1c75e83f14443f9469cfa35aa5bb29fee
Jinoh Kim <juju08217@daum.net>
2022-06-05 22:47:40 +0900

    section8 DP 9 solved again. 답지 풀이. 이렇게 하니까 훨씬 코드가 줄음. 직관적이지는 않지만, 그만큼 문제를 잘 이해하고 있다고 생각할 수 있을듯

--- inflearn_lecture/8_dynamic_programming/09_knapsack_algorithm.py
+++ inflearn_lecture/8_dynamic_programming/09_knapsack_algorithm.py
@@ -7,15 +7,11 @@ def solution(**kwargs):
         sys.stdin = open(f"{kwargs['folder_path']}/in{kwargs['id']}.txt", 'r')
 
     N, limit = [int(num) for num in input().split()]
-    jewelry_list = []
-    for i in range(N):
-        jewelry_list.append([int(num) for num in input().split()])
-
     knapsack = [0] * (limit + 1)
-    for i in range(1, len(knapsack)):
-        for weight, price in jewelry_list:
-            if i >= weight:
-                knapsack[i] = max(knapsack[i - weight] + price, knapsack[i])
+    for i in range(N):
+        weight, price = [int(num) for num in input().split()]
+        for i in range(weight, len(knapsack)):
+            knapsack[i] = max(knapsack[i - weight] + price, knapsack[i])
 
     return [str(knapsack[-1])]
 
