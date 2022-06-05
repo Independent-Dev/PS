4123528f9b441b38984a800856e61aa72b530fb3
Jinoh Kim <juju08217@daum.net>
2022-06-05 22:20:30 +0900

    section8 DP 9 solved

---
+++ inflearn_lecture/8_dynamic_programming/09_knapsack_algorithm.py
@@ -0,0 +1,23 @@
+from examine import examine
+import sys, math
+
+@examine
+def solution(**kwargs):
+    if kwargs['examine']:
+        sys.stdin = open(f"{kwargs['folder_path']}/in{kwargs['id']}.txt", 'r')
+
+    N, limit = [int(num) for num in input().split()]
+    jewelry_list = []
+    for i in range(N):
+        jewelry_list.append([int(num) for num in input().split()])
+
+    knapsack = [0] * (limit + 1)
+    for i in range(1, len(knapsack)):
+        for weight, price in jewelry_list:
+            if i >= weight:
+                knapsack[i] = max(knapsack[i - weight] + price, knapsack[i])
+
+    return [str(max(knapsack))]
+
+if __name__ == "__main__":
+    print(solution(examine=True))
